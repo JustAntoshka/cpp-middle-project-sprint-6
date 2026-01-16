0d75b041ee01c5065947f80c3a7c06f2c5c966b9
JustAntoshka <anton.tyugaevich@gmail.com>
2026-01-16 23:15:24 +0000

    implement thread pool

--- include/thread_pool/thread_pool.hpp
+++ include/thread_pool/thread_pool.hpp
@@ -1,9 +1,22 @@
 #pragma once
 
+#include "queue/priority_queue.hpp"
+#include <memory>
+
 namespace dispatcher::thread_pool {
 
 class ThreadPool {
-  // здесь ваш код
+public:
+    ThreadPool(std::size_t thread_count, std::shared_ptr<dispatcher::queue::PriorityQueue> queue);
+
+    ~ThreadPool();
+
+private:
+    void worker();
+
+private:
+    std::shared_ptr<dispatcher::queue::PriorityQueue> queue_;
+    std::vector<std::thread> workers_;
 };
 
-} // namespace dispatcher::thread_pool
+}  // namespace dispatcher::thread_pool
--- src/thread_pool/thread_pool.cpp
+++ src/thread_pool/thread_pool.cpp
@@ -2,6 +2,36 @@
 
 namespace dispatcher::thread_pool {
 
-// здесь ваш код
+ThreadPool::ThreadPool(std::size_t thread_count, std::shared_ptr<dispatcher::queue::PriorityQueue> queue)
+    : queue_(std::move(queue)) {
 
-} // namespace dispatcher::thread_pool
+    workers_.reserve(thread_count);
+
+    for (std::size_t i = 0; i < thread_count; ++i) {
+        workers_.emplace_back(&ThreadPool::worker, this);
+    }
+}
+
+ThreadPool::~ThreadPool() {
+    if (queue_) {
+        queue_->shutdown();
+    }
+
+    for (auto &w : workers_) {
+        if (w.joinable()) {
+            w.join();
+        }
+    }
+}
+
+void ThreadPool::worker() {
+    while (true) {
+        auto task = queue_->pop();
+        if (!task) {
+            return;
+        }
+        (*task)();
+    }
+}
+
+}  // namespace dispatcher::thread_pool
